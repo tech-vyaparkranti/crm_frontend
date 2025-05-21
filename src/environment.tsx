d4ca1393dcabce7d501dde6c42bc4fbc59c1a4a2
abhinesh03 <abhinesh.yadav@vyaparkranti.in>
2025-05-21 11:26:57 +0530

    call-log

--- src/environment.tsx
+++ src/environment.tsx
@@ -1,3 +1,3 @@
-export const base_path = 'http://192.168.0.193:8000'; // ✅ for API only
+export const base_path = 'http://192.168.1.5:8000'; // ✅ for API only
 export const route_path = '/';
 export const img_path = '/';
